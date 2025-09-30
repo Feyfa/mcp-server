763eb8761bd77905e6b04385deb6880a7aa4ac60
Muhammad Jidan <muhammadjidan703@gmail.com>
2025-10-01 03:11:44 +0700

    learn mcp server

---
+++ src/client.ts
@@ -0,0 +1,122 @@
+/* IMPORT MODULE */
+import "dotenv/config"
+import { createGoogleGenerativeAI } from "@ai-sdk/google" // module ini bisa di analogikan seperti driver database di laravel
+import { confirm, input, select } from "@inquirer/prompts"
+import { Client } from "@modelcontextprotocol/sdk/client/index.js"
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js"
+import { CreateMessageRequestSchema, Prompt, PromptMessage, Tool } from "@modelcontextprotocol/sdk/types.js"
+import { generateText, jsonSchema, ToolSet } from "ai" // module ini bisa di analogikan seperti orm di laravel
+/* IMPORT MODULE */
+
+
+/* SETUP MCP CLIENT */
+const mcpClient = new Client(
+    {
+        name: "text-client-video",
+        version: "1.0.0"
+    },
+    { capabilities: { sampling: {} } }
+)
+/* SETUP MCP CLIENT */
+
+
+/* SETUP CLIENT TRANSPORT */
+// ini adalah cara komunikasi antara mcp client dengan mcp server
+const transport = new StdioClientTransport({
+    command: "node",
+    args: ["build/server.js"],
+    stderr: "ignore"
+})
+/* SETUP CLIENT TRANSPORT */
+
+
+/* SETUP LLM GEMINI */
+const google = createGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY || "",
+})
+/* SETUP LLM GEMINI */
+
+
+/* EXTEND FUNCTION */
+async function handleServerMessagePrompt(message: PromptMessage) {
+    if (message.content.type !== "text") 
+        return;
+
+    console.log(message.content.text)
+    const run = await confirm({
+        message: "Would you like to run the above prompt",
+        default: true,
+    })
+
+    if (!run) return
+
+    const { text } = await generateText({
+        model: google("gemini-2.0-flash"),
+        prompt: message.content.text,
+    })
+
+    return text
+}
+
+async function handleQuery(tools: Tool[]) {
+    const query = await input({ message: "Enter your query" })
+
+    const toolsFormat: ToolSet = {}
+    for (const tool of tools) {
+        toolsFormat[tool.name] = {
+            description: tool.description,
+            inputSchema: jsonSchema(tool.inputSchema),
+            execute: async (args: Record<string, any>) => {
+                return await mcpClient.callTool({
+                    name: tool.name,
+                    arguments: args,
+                })
+            },
+        }
+    }
+
+    const { toolResults } = await generateText({
+        model: google("gemini-2.0-flash"),
+        prompt: query,
+        tools: toolsFormat,
+    })
+
+    console.log(toolResults[0]?.output?.content[0]?.text || "No text generated.")
+}
+/* EXTEND FUNCTION */
+
+
+/* SETUP MAIN */
+async function main() {
+    await mcpClient.connect(transport) // untuk connect ke mcp server
+
+    const [{ tools } ] = await Promise.all([
+        mcpClient.listTools(),
+    ]); // untuk mengambil daftar kemampuan yang ada di mcp server
+
+    mcpClient.setRequestHandler(CreateMessageRequestSchema, async request => {
+        const texts: string[] = []
+        for (const message of request.params.messages) {
+            const text = await handleServerMessagePrompt(message)
+            if(text != null)
+                texts.push(text)
+        }
+
+        return {
+            role: "user",
+            model: "gemini-2.0-flash",
+            stopReason: "endTurn",
+            content: {
+                type: "text",
+                text: texts.join("\n")
+            }
+        }
+    })
+
+    while (true) {
+        await handleQuery(tools)
+    }
+}
+main()
+/* SETUP MAIN */
+
--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,47 @@
+/* IMPORT MODULE */
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 import z from "zod"
 import fs from "node:fs/promises"
+/* IMPORT MODULE */
 
-const server = new McpServer({
-    name: "test",
+
+/* SETUP MCP SERVER */
+const mcpServer = new McpServer({
+    name: "test-video",
     version: "1.0.0",
-    capabilities: {
-        resources: {},
-        tools: {},
-        prompts: {},
-    }
+    capabilities: { tools: {} }
 })
+/* SETUP MCP SERVER */
 
-server.tool(
-    "create-user", 
-    "Create a new user in the database", 
-    {
+
+/* LIST TOOL MCP */
+mcpServer.tool(
+    "create-user", // ini adalah slug dari tool
+    "Create a new user in the database", // ini adalah deskripsi tool, ini yang membuat llm paham bahwa tujuan dari tool ini tuh untuk create user
+    { // ini adalah schema dan validator dari parameter yang dibutuhkan oleh tool ini
         name: z.string(),
         email: z.string(),
+        phone: z.string(),
         address: z.string(),
-        phone: z.string()
     }, 
     {
-        title: 'Create User',
-        readOnlyHint: false,
-        destructiveHint: false,
-        idempotentHint: false,
-        openWorldHint: true
+        title: 'Create User', // judul human-friendly untuk tool.
+        readOnlyHint: false, // true -> Tool ini hanya membaca data, tidak mengubah state | false -> Tool ini mengubah state/data (insert, update, delete)
+        destructiveHint: false, // true -> Tool ini bisa menghapus/merusak data (destructive) | false -> Aman, tidak menghapus data
+        idempotentHint: false, // true -> Panggil berulang kali hasil akhirnya sama | false -> Panggil berulang kali hasilnya beda.
+        openWorldHint: false, // true -> AI boleh “ngarang” input tambahan di luar schema | false -> Input harus persis sesuai schema, tidak boleh ada field lain
     }, 
-    async (params) => {
+    async (params) => { // event yang dijalankan ketika tool ini dipanggil
         try {
             const id = await createUser(params)
-            return {
+            return { // format return mcp ini tidak boleh asal-asalan
                 content: [
                     { type: "text", text: `User ${id} created successfully` }
                 ]
             }
         } catch (error) {
-            return {
+            return { // format return mcp tidak boleh asal-asalan
                 content: [
                     { type: "text", text: `Failed to save user, message: ${error}` }
                 ]
@@ -46,29 +49,35 @@ server.tool(
         }
     }
 )
+/* LIST TOOL MCP */
 
-async function createUser(user: {
+
+/* FUNCTION CREATE USER */
+type User = {
+    id: Number,
     name: string,
     email: string,
     address: string,
     phone: string
-}) {
+}
+async function createUser(user: Omit<User, "id">) {
     const users = await import("./data/users.json", {
         with: { type: "json" }
-    }).then(m => m.default)
+    }).then(m => m.default) as User[]
 
     const id = users.length + 1
-
     users.push({ id, ...user })
 
     await fs.writeFile("./src/data/users.json", JSON.stringify(users, null, 4))
-
     return id
 }
+/* FUNCTION CREATE USER */
 
+
+/* FUNCTION MAIN */
 async function main() {
     const transport = new StdioServerTransport()
-    await server.connect(transport)
+    await mcpServer.connect(transport)
 }
-
 main()
+/* FUNCTION MAIN */
